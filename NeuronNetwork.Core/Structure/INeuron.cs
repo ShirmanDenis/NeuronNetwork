2076effb4ac736d8767dc14342c206f6e246ebdd
Denis <denshi747@gmail.com>
2017-07-12 23:12:19 +0500

    WIP

---
+++ NeuronNetwork.Core/Structure/INeuron.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NeuronNetwork.Core.Structure;
+
+namespace NeuronNetwork.Core
+{
+    interface INeuron
+    {
+        IWeight<object>[,] Weights { get; set; }
+    }
+}
---
+++ NeuronNetwork.Core/Structure/NeuronBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuronNetwork.Core.Structure
+{
+    class NeuronBase : INeuron
+    {
+        public IWeight<object>[,] Weights { get; set; }
+
+        public NeuronBase(IWeight<object>[,] weights)
+        {
+            Weights = weights;
+        }
+    }
+}
