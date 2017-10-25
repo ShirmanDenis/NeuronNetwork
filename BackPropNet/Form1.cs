12cda9a3dfc938c607be327c59879d01bd729407
Denis <denshi747@gmail.com>
2017-10-25 11:31:20 +0500

    Try Aforge BackProp

---
+++ BackPropNet/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Neuro;
+using AForge.Neuro.Learning;
+
+namespace BackPropNet
+{
+    public partial class Form1 : Form
+    {
+        private bool needToStop = false;
+        private ActivationNetwork neuronNet;
+        private Thread workThread;
+        private BackPropagationLearning teacher;
+        private SampleStorage sampleStorage;
+        private string errorVal = "";
+        public Form1()
+        {
+            InitializeComponent();
+
+            sampleStorage = new SampleStorage();
+            sampleStorage.ReadFromFile("dataSet.txt");
+
+            //  Network Params
+
+            var alpha = 2;
+
+            neuronNet = new ActivationNetwork(new SigmoidFunction(alpha), 8, 8, 1);
+            neuronNet.Randomize();
+            teacher = new BackPropagationLearning(neuronNet) { LearningRate = 1, Momentum = 0.1 };
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            needToStop = false;
+            workThread = new Thread(() =>
+            {
+                // Learning
+                while (!needToStop)
+                {
+                    var error = teacher.RunEpoch(sampleStorage.InputLearn, sampleStorage.OutPutLearn);
+                    errorVal = error.ToString(CultureInfo.InvariantCulture);
+                    if (error < 0.1)
+                        needToStop = true;
+                }
+            });
+            workThread.Priority =ThreadPriority.Highest;
+            workThread.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            neuronNet.Save("Weights_.txt");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            textBox1.Text = errorVal;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            needToStop = true;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (workThread.ThreadState != ThreadState.Running) return;
+            needToStop = true;
+            workThread.Join();
+            workThread.Abort();
+        }
+    }
+}
---
+++ BackPropNet/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Neuro;
+using AForge.Neuro.Learning;
+
+namespace BackPropNet
+{
+    class SampleStorage
+    {
+        public double[][] InputLearn { get; private set; }
+        public double[][] OutPutLearn { get; private set; }
+
+        public double[][] InputWork { get; private set; }
+        public double[][] OutputWork { get; private set; }
+        public void ReadFromFile(string fileName)
+        {
+            var inputList = new List<double[]>();
+            var outPutList = new List<double[]>();
+            foreach (var samples in File.ReadLines(fileName))
+            {
+                var parsedValues = samples.Split(',').Select(elem => elem.Length == 1 ? 
+                                                                elem.ToCharArray().First() :
+                                                                double.Parse(elem, CultureInfo.InvariantCulture)).ToList();
+                var input = parsedValues.GetRange(0, 8).ToArray();
+                inputList.Add(input);
+                var output = new[] { parsedValues.Last()};
+                outPutList.Add(output);
+            }
+
+            var learnCount = (int)Math.Floor(inputList.Count * 0.2);
+            var workCount = inputList.Count - learnCount;
+
+            // Normalize values
+            inputList.ForEach(elem =>
+            {
+                for (var i = 0; i < elem.Length; i++)
+                    elem[i] = elem[i] / 100.0;
+            });
+            outPutList.ForEach(elem =>
+            {
+                for (var i = 0; i < elem.Length; i++)
+                    elem[i] = elem[i] / 100.0;
+            });
+            // Devide samples to Learning and Working sequences
+            InputLearn = inputList.GetRange(0, learnCount).ToArray();
+            OutPutLearn = outPutList.GetRange(0, learnCount).ToArray();
+            InputWork = inputList.GetRange(learnCount - 1, workCount).ToArray();
+            OutputWork = outPutList.GetRange(learnCount - 1, workCount).ToArray();
+        }
+    }
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
